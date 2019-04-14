8527d3e4ee81a76d7bacf10fbc86046c8ce3f9e6
LouisPetrik <louispetrik@aol.com>
2019-04-14 21:40:17 +0200

    Updates

---
+++ precache-manifest.0cf59aa556c16244bec77ecb2a0d0970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e970dd138d2748c54886869cbcde201",
+    "url": "/react-blog-boilerplate/static/media/sunset.4e970dd1.jpg"
+  },
+  {
+    "revision": "6d3b130f6f138fae90a0",
+    "url": "/react-blog-boilerplate/static/js/runtime~main.69b17513.js"
+  },
+  {
+    "revision": "04fb5fe6f2294f8ef513",
+    "url": "/react-blog-boilerplate/static/js/main.7e661122.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/react-blog-boilerplate/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "04fb5fe6f2294f8ef513",
+    "url": "/react-blog-boilerplate/static/css/main.4d03c8e5.chunk.css"
+  },
+  {
+    "revision": "a3b545fbc62d90430321178d3bdb6590",
+    "url": "/react-blog-boilerplate/index.html"
+  }
+];
